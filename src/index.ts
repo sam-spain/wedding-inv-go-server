69e1ae750f7dcb06754f7494ced00e4e025c3a99
sam <sam@samspain.com>
2021-09-19 21:44:20 +0100

    Handle broken promises

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,13 @@ const PORT = process.env.PORT || 8080; // default port to listen
 app.use("/api/v1/invitee", inviteeRouter);
 
 // start the Express server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at http://localhost:${PORT}`);
+});
+
+process.on("unhandledRejection", (err: any, promise) => {
+  // tslint:disable-next-line:no-console
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
