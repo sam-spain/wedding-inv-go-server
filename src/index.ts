5d16a53a82ff7f1e071cfdc0b06d1889abd1d9d6
sam <sam@samspain.com>
2021-09-19 17:54:34 +0100

    Create invitee routes in root file

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,25 @@ app.get("/", (req, res) => {
   res.send("Hello user");
 });
 
+app.get("/api/v1/invitee", (req, res) => {
+  res.status(200).json([{name: "sam"}, {name: "karen"}]);
+})
+
+app.get("/api/v1/invitee/:id", (req, res) => {
+  res.status(200).json({id: req.params.id, name: "sam"});
+})
+
+app.post("/api/v1/invitee", (req, res) => {
+  res.status(200).json({"created invitee" : {name: "sam"}});
+})
+
+app.put("/api/v1/invitee/:id", (req, res) => {
+  res.status(200).json({"modified invitee" : {id: req.params.id, name: "sam"}});
+})
+
+app.delete("/api/v1/invitee/:id", (req, res) => {
+  res.status(200).json({"deleted invitee" : {id: req.params.id, name: "sam"}});
+})
 // start the Express server
 app.listen(PORT, () => {
   // tslint:disable-next-line:no-console
