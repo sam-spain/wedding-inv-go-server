915166b5075507eb45e1fd93c9a6553fdc08b63c
sam <sam@samspain.com>
2021-09-20 22:31:33 +0100

    Test run

---
+++ tests/controllers/inviteeControllerTest.ts
@@ -0,0 +1,10 @@
+import { getInvitees } from '../../src/controllers/inviteeController';
+import { expect } from 'chai';
+import Request from 'supertest';
+
+
+describe("Get Invitees", () => {
+    it("Should return list of invitees", () => {
+        expect(true).to.be.true;
+    })
+})
