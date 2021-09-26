f80514a317ca9b4c0f2fbe0b0d052444ffcadb7c
Sam Spain <sam@samspain.com>
2021-09-26 12:49:29 +0100

    Add basic Invitee endpoints (#3)

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -34,9 +34,65 @@ describe("Invitee API Endpoint", () => {
     })
     return Request(Server)
     .get('/api/v1/invitee')
-    .expect({message: 'Failed to retrieve invitees.'})
+    .expect({})
     .expect(500);
   })
+
+  
+  test('Respond to GET by ID with invitee and 200', () => {
+    InviteeModel.findById = jest.fn().mockResolvedValue(samSpainInvitee);
+    return Request(Server)
+    .get('/api/v1/invitee/1')
+    .expect(samSpainInvitee)
+    .expect(200);
+  })
+  
+  test('Respond to failed GET by ID with 500', () => {
+    InviteeModel.findById = jest.fn().mockImplementation(() => {
+      throw new Error();
+    });
+    return Request(Server)
+    .get('/api/v1/invitee/1')
+    .expect({})
+    .expect(500);
+  });
+  
+  test('Respond to empty GET by ID with 404', () => {
+    InviteeModel.findById = jest.fn().mockResolvedValue(null);
+    return Request(Server)
+    .get('/api/v1/invitee/1')
+    .expect({message: 'No invitee with matching ID found.'})
+    .expect(404);
+  });
+  
+  test('Respond to PUT with ID with updated invitee and 200', () => {
+    InviteeModel.findByIdAndUpdate = jest.fn().mockResolvedValue(samSpainInvitee);
+    const updatedInvitee = {
+      updatedInvitee: samSpainInvitee,
+    };
+    return Request(Server)
+      .put('/api/v1/invitee/1')
+      .expect(updatedInvitee)
+      .expect(200);
+  });
+  
+  test('Respond to PUT with ID with 404 when failed to find', () => {
+    InviteeModel.findByIdAndUpdate = jest.fn().mockResolvedValue(null);
+    return Request(Server)
+      .put('/api/v1/invitee/1')
+      .expect(404)
+      .expect({message: 'Failed to find invitee to update.'});
+  });
+
+  test('Respond to PUT with ID with 500 when failed', () => {
+    InviteeModel.findByIdAndUpdate = jest.fn().mockImplementation(() => {
+      throw new Error();
+    });
+    return Request(Server)
+      .put('/api/v1/invitee/1')
+      .expect(500)
+      .expect({});
+  });
 
   test('Respond to POST with created invitee and 201', () => {
     InviteeModel.create = jest.fn().mockResolvedValue(samSpainInvitee);
@@ -45,15 +101,41 @@ describe("Invitee API Endpoint", () => {
       .expect(201)
       .expect(samSpainInvitee);
   });
-
+  
   test('Respond to failed POST with 500', () => {
     InviteeModel.create = jest.fn().mockImplementation(() => {
       throw new Error();
     });
     return Request(Server)
-      .post('/api/v1/invitee')
-      .expect(500)
-      .expect({err: {}});
+    .post('/api/v1/invitee')
+    .expect(500)
+    .expect({});
   });
 
+  test('Respond to DELETE with ID with empty and 204', () => {
+    InviteeModel.findByIdAndDelete = jest.fn().mockResolvedValue(samSpainInvitee);
+    return Request(Server)
+      .delete('/api/v1/invitee/1')
+      .expect(204)
+      .expect({});
+  });
+
+  test('Respond to DELETE where invitee could not be found with 404', () => {
+    InviteeModel.findByIdAndDelete = jest.fn().mockResolvedValue(null);
+    return Request(Server)
+      .delete('/api/v1/invitee/1')
+      .expect(404)
+      .expect({message: 'Failed to find invitee to delete.'});
+  });
+
+  test('Respond to DELETE that failed with 500', () => {
+    InviteeModel.findByIdAndDelete = jest.fn().mockImplementation(() => {
+      throw new Error();
+    });
+    return Request(Server)
+      .delete('/api/v1/invitee/1')
+      .expect(500)
+      .expect({});
+  });
+  
 });
