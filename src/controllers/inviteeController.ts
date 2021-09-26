3053c182893fcd041a85c7824198c0f8b9c86c60
sam <sam@samspain.com>
2021-09-26 12:42:45 +0100

    Add basic endpoints with tests

--- src/controllers/inviteeController.ts
+++ src/controllers/inviteeController.ts
@@ -7,14 +7,21 @@ export async function getInvitees(req : any, res: any, next: any) {
         const invitees = await InviteeModel.find();
         res.status(200).json(invitees);
     } catch (err) {
-        res.status(500).json({message: 'Failed to retrieve invitees.'});
+        res.status(500).json(err);
     }
 }
 
 // @description Get person invited by ID
 // @route       GET /api/v1/invitee/:id
-export function getInvitee(req: any, res: any, next: any) {
-    res.status(200).json({ id: req.params.id, name: "sam" });
+export async function getInvitee(req: any, res: any, next: any) {
+    try {
+        const foundInvitee = await InviteeModel.findById(req.params.id);
+        if(foundInvitee)
+            res.status(200).json(foundInvitee);
+        else res.status(404).json({message: 'No invitee with matching ID found.'});
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 
 // @description Invite new person
@@ -24,22 +31,44 @@ export async function createInvitee(req: any, res: any, next: any) {
         const newInvitee = await InviteeModel.create(req.body);
         res.status(201).json(newInvitee);
     } catch (err) {
-        res.status(500).json({err});
+        res.status(500).json(err);
     }
 }
 
 // @description Update existing invitee
 // @route       PUT /api/v1/invitee/:id
-export function updateInvitee(req: any, res: any, next: any) {
-    res
-    .status(200)
-    .json({ "modified invitee": { id: req.params.id, name: "sam" } });
+export async function updateInvitee(req: any, res: any, next: any) {
+    try {
+        const updatedInvitee = await InviteeModel.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          {
+            new: true,
+            runValidators: true,
+          },
+        );
+
+        if (!updatedInvitee)
+          return res
+            .status(404)
+            .json({message: 'Failed to find invitee to update.'});
+        else return res.status(200).json({updatedInvitee});
+      } catch (err) {
+        return res.status(500).json(err);
+      }
 }
 
 // @description Delete existing invitee
 // @route       DELETE /api/v1/invitee/:id
-export function deleteInvitee (req: any, res: any, next: any) {
-    res
-    .status(200)
-    .json({ "deleted invitee": { id: req.params.id, name: "sam" } });
+export async function deleteInvitee (req: any, res: any, next: any) {
+    try {
+        const deletedInvitee = await InviteeModel.findByIdAndDelete(req.params.id);
+        if (!deletedInvitee)
+          return res
+            .status(404)
+            .json({message: 'Failed to find invitee to delete.'});
+        else return res.status(204).json({});
+      } catch (err) {
+        return res.status(500).json(err);
+      }
 }
