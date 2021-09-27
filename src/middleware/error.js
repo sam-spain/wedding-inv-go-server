c131abb2b255b392f385b07f7109daafef010f70
sam <sam@samspain.com>
2021-09-27 23:59:27 +0100

    Refactor error class. Auto format and remove unused class

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,56 +1,51 @@
-const ErrorResponse = require("../util/errorResponse");
-
 const errorHandler = (inputError, req, res, next) => {
   console.log("Error handler");
-  let defaultError = {...inputError}
+  let defaultError = { ...inputError };
   defaultError.message = inputError.message;
 
   let newErrorResponse = undefined;
-  
-  // Mongoose bad ObjectId
-  if(inputError.name === "CastError") {
+
+  if (inputError.name === "CastError") {
     newErrorResponse = {
       statusCode: 404,
       response: {
         errorType: "ClientFail",
-        content: `Resource not found with id of ${inputError.value}`
-      }
-    }
+        content: `Resource not found with id of ${inputError.value}`,
+      },
+    };
   }
 
-  if(inputError.code === 11000) {
+  if (inputError.code === 11000) {
     newErrorResponse = {
       statusCode: 409,
       response: {
         errorType: "ClientFail",
-        content: "Duplicate field value entered"
-      }
-    }
+        content: "Duplicate field value entered",
+      },
+    };
   }
 
-
-  // Mongoose validation error
-  if(inputError.name === 'ValidationError') {
+  if (inputError.name === "ValidationError") {
     console.log(inputError.errors);
     newErrorResponse = {
       statusCode: 422,
       response: {
         errorType: "validation",
-        content: Object.entries(inputError.errors).map(error => {
+        content: Object.entries(inputError.errors).map((error) => {
           let property = error[1].properties;
-          return {[property.path]: property.message};
-        })
-      }
-    }
+          return { [property.path]: property.message };
+        }),
+      },
+    };
   }
 
-  if(newErrorResponse) {
+  if (newErrorResponse) {
     res.status(newErrorResponse.statusCode).json(newErrorResponse.response);
   } else {
     res.status(500).json({
       errorType: "Unknown",
-      content: "An unexpected server error occurred"
-    })
+      content: "An unexpected server error occurred",
+    });
   }
 };
 
