7a36c33a15e7f32273d7d28d46b0b4f0a1cfc949
sam <sam@samspain.com>
2021-10-30 12:08:16 +0100

    Use query params to filter invitees

--- src/controllers/inviteeController.js
+++ src/controllers/inviteeController.js
@@ -5,55 +5,49 @@ const asyncHandler = require("../middleware/async");
 // @description Get all people invited
 // @route       GET /api/v1/invitee
 exports.getInvitees = asyncHandler(async (req, res, next) => {
-    const invitees = await Invitee.find();
-    res.status(200).json(invitees);
+  const invitees = await Invitee.find(req.query);
+  res.status(200).json(invitees);
 });
 
 // @description Get person invited by ID
 // @route       GET /api/v1/invitee/:id
 exports.getInvitee = asyncHandler(async (req, res, next) => {
-    const foundInvitee = await Invitee.findById(req.params.id);
-    if (foundInvitee) res.status(200).json(foundInvitee);
-    else
-    return next(
-      new ErrorResponse("Invitee", "NotFoundError", req.params.id)
-    );
+  const foundInvitee = await Invitee.findById(req.params.id);
+  if (foundInvitee) res.status(200).json(foundInvitee);
+  else
+    return next(new ErrorResponse("Invitee", "NotFoundError", req.params.id));
 });
 
 // @description Invite new person
 // @route       POST /api/v1/invitee
 exports.createInvitee = asyncHandler(async (req, res, next) => {
-  console.log("Create")
-    const newInvitee = await Invitee.create(req.body);
-    res.status(201).json(newInvitee);
+  console.log("Create");
+  const newInvitee = await Invitee.create(req.body);
+  res.status(201).json(newInvitee);
 });
 
 // @description Update existing invitee
 // @route       PUT /api/v1/invitee/:id
 exports.updateInvitee = asyncHandler(async (req, res, next) => {
-    const updatedInvitee = await Invitee.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+  const updatedInvitee = await Invitee.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
-    if (updatedInvitee)
-    return res.status(200).json({ updatedInvitee })
-      else 
-      return next(
-        new ErrorResponse("Invitee", "NotFoundError", req.params.id)
-      );
+  if (updatedInvitee) return res.status(200).json({ updatedInvitee });
+  else
+    return next(new ErrorResponse("Invitee", "NotFoundError", req.params.id));
 });
 
 // @description Delete existing invitee
 // @route       DELETE /api/v1/invitee/:id
 exports.deleteInvitee = asyncHandler(async (req, res, next) => {
-    const deletedInvitee = await Invitee.findByIdAndDelete(req.params.id);
-    if (deletedInvitee)
-    return res.status(204).json({});
-    else
+  const deletedInvitee = await Invitee.findByIdAndDelete(req.params.id);
+  if (deletedInvitee) return res.status(204).json({});
+  else
     return next(new ErrorResponse("Invitee", "NotFoundError", req.params.id));
 });
