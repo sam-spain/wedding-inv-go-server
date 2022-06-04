733ff482bb4b45d612d39e895f4a1ced748bc8c2
sam <sam@samspain.com>
2022-06-04 08:45:19 +0100

    Add user model.

---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const UserSchema = -new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+  },
+  role: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minLength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
