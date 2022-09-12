5daaf77eb395045775a0b22f22ebd6a8df41acab
sam <sam@samspain.com>
2022-09-13 00:49:23 +0100

    Use https server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const Server = require("./server");
 const dotenv = require("dotenv");
 const PORT = process.env.PORT || 8080;
+const https = require("https");
+const fs = require("fs");
+const privateKey = fs.readFileSync(process.env.SSL_KEY_LOCATION, "utf8");
+const certificate = fs.readFileSync(
+  process.env.SSL_CERTIFICATE_LOCATION,
+  "utf8"
+);
+const ca = fs.readFileSync(process.env.SSL_CHAIN_LOCATION, "utf8");
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+const httpsServer = https.createServer(credentials, Server);
+
 const { connectDb } = require("./config/db");
 dotenv.config();
 
@@ -11,7 +26,7 @@ Server.use((req, res, next) => {
   next();
 });
 
-const server = Server.listen(PORT, () => {
+const server = httpsServer.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
 });
 
