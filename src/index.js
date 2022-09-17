4c8fb03aef79e295d22e49877ec66da75ca16166
Sam Spain <sam@samspain.com>
2022-09-17 23:35:53 +0100

    Only use https when in production

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ const dotenv = require("dotenv");
 const PORT = process.env.PORT || 8080;
 const https = require("https");
 const fs = require("fs");
-const privateKey = fs.readFileSync(process.env.SSL_KEY_LOCATION, "utf8");
-const certificate = fs.readFileSync(
-  process.env.SSL_CERTIFICATE_LOCATION,
-  "utf8"
-);
-const ca = fs.readFileSync(process.env.SSL_CHAIN_LOCATION, "utf8");
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
-const httpsServer = https.createServer(credentials, Server);
+if(process.env.NODE_ENV === "production") {
+  const privateKey = fs.readFileSync(process.env.SSL_KEY_LOCATION, "utf8");
+  const certificate = fs.readFileSync(
+    process.env.SSL_CERTIFICATE_LOCATION,
+    "utf8"
+  );
+  const ca = fs.readFileSync(process.env.SSL_CHAIN_LOCATION, "utf8");
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+  };
+  const httpsServer = https.createServer(credentials, Server);
+}
 
 const { connectDb } = require("./config/db");
 dotenv.config();
@@ -26,9 +28,16 @@ Server.use((req, res, next) => {
   next();
 });
 
-const server = httpsServer.listen(PORT, () => {
-  console.log(`server started at http://localhost:${PORT}`);
-});
+if(process.env.NODE_ENV === "production") {
+  const server = httpsServer.listen(PORT, () => {
+    console.log(`Secure server started at localhost:${PORT}`);
+  });
+} else {
+  const server = Server.listen(PORT, () => {
+    console.log(`server started at localhost:${PORT}`);
+  });
+}
+
 
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`);
